7a1838ba79b8aae43a8f6722679bf896870bb4cc
S-a-l-a-d <xa.lach@yahoo.com>
2018-03-04 14:51:22 +0700

    Added repo link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,6 +7,12 @@ export default class HeaderComponent extends React.Component {
       <header className="App-header">
         <Logo />
         <h1 className="App-title">Welcome to React</h1>
+        Visit my repo at <a
+          className="repo"
+          href="https://github.com/S-a-l-a-d/react-quick-start"
+          target="_blank"
+        >S-a-l-a-d/react-quick-start
+        </a>
       </header>
     );
   }
